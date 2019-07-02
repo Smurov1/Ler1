103521c8c169080d6a4f76296e7cace169234e00
Smurov1 <crapyourdick@gmail.com>
2019-07-02 05:59:47 +0300

    all

--- ler2.py
+++ ler2.py
@@ -6,23 +6,23 @@ bot = telebot.TeleBot("752580762:AAG4dwXG4BpjPJ14fMlUnDo2NvtfpAiRv_E")
 def get_text_messages(message):
     if message.text == "я ебал твою тёлку":
         bot.send_message(message.chat.id, "у!")
-    elif message.text == "как работа?"
+    elif message.text == "как работа?":
         bot.send_message(message.chat.id, "пиарщики заебали")
-    elif message.text == "как дел?"
+    elif message.text == "как дел?":
         bot.send_message (message.chat.id, "жиза говно")
-    elif message.text == "смотри какая тачка"
+    elif message.text == "смотри какая тачка":
         bot.send_message (message.chat.id, "ору")
-    elif message.text == "че делаешь"
+    elif message.text == "че делаешь":
         bot.send_message(message.chat.id, "лежу")
-    elif message.text == "что самое вкусное на свете?"
+    elif message.text == "что самое вкусное на свете?":
         bot.send_message(message.chat.id, "чипсики")
-    elif message.text == "вот бы"
+    elif message.text == "вот бы":
         bot.send_message(message.chat.id, "жирной хавки")
-    elif message.text == "что такое vc?"
+    elif message.text == "что такое vc?":
         bot.send_message (message.chat.id, "TJournal знаешь?")
     elif message.text == "говно" or message.text == "уныло" or message.text == "жиза отстой"
         bot.send.message (message.chat.id, "ну ты шо")
-    elif message.text == "бля пиздец"
+    elif message.text == "бля пиздец":
         bot.send_message(message.chat.id, "ёбаный рот этого казино")
 
 bot.polling(none_stop=true, interval=0)
