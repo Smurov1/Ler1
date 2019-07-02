cf1c040cb83c46157523218c397ed9a2ed1993a1
Smurov1 <crapyourdick@gmail.com>
2019-07-02 06:10:17 +0300

    all

--- ler2.py
+++ ler2.py
@@ -8,22 +8,24 @@ def get_text_messages(message):
         bot.send_message(message.chat.id, "у!")
     elif message.text == "как работа?":
         bot.send_message(message.chat.id, "пиарщики заебали")
-    elif message.text == "как дел?":
-        bot.send_message (message.chat.id, "жиза говно")
+    if message.text == "как дел?":
+        bot.send_message(message.chat.id, "жиза говно")
     elif message.text == "смотри какая тачка":
-        bot.send_message (message.chat.id, "ору")
-    elif message.text == "че делаешь":
+        bot.send_message(message.chat.id, "ору")
+    if message.text == "че делаешь":
         bot.send_message(message.chat.id, "лежу")
-    elif message.text == "что самое вкусное на свете?":
+    if message.text == "что самое вкусное на свете?":
         bot.send_message(message.chat.id, "чипсики")
     elif message.text == "вот бы":
         bot.send_message(message.chat.id, "жирной хавки")
-    elif message.text == "что такое vc?":
-        bot.send_message (message.chat.id, "TJournal знаешь?")
-    elif message.text == "говно" or message.text == "уныло" or message.text == "жиза отстой"
-        bot.send.message (message.chat.id, "ну ты шо")
-    elif message.text == "бля пиздец":
+    if message.text == "что такое vc?":
+        bot.send_message(message.chat.id, "TJournal знаешь?")
+    elif message.text == "говно" or message.text == "уныло" or message.text == "жиза отстой":
+        bot.send.message(message.chat.id, "ну ты шо")
+    if message.text == "бля пиздец":
         bot.send_message(message.chat.id, "ёбаный рот этого казино")
+    elif message.text == "кто самый крутой рэпер?":
+        bot.send_message(message.chat.id, "оксимирон")
 
 bot.polling(none_stop=true, interval=0)
 
